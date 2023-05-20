302aa4d9deb50bda130f2fd709418d9b8c9c93ca
JackHao0921 <jack02131166@gmail.com>
2023-05-21 02:12:36 +0800

    修正ＲＷＤ導覽列白底ＢＵＧ

--- toggle.js
+++ toggle.js
@@ -1,10 +1,9 @@
-$(function(){
-    $(".TOGGLE").click(function(){
-        $('.TOGGLE > span').toggleClass('close');
-        $('.TOGGLE').toggleClass('close');
-        $(".M_HEADER_NAV > ul").slideToggle();
- 
-
-
-    });
-});
+// $(function(){
+//     $(".TOGGLE").click(function(){
+//         $('.TOGGLE > span').toggleClass('close');
+//         $('.TOGGLE').toggleClass('close');
+//         $(".M_HEADER_NAV > ul").slideToggle();
+//         $(".HEADER_NAV, .M_HEADER_NAV_BG").removeClass('HEADER_NAV_SHADOW');
+//         $(".M_HEADER_NAV_BG").removeClass('M_HEADER_BG_ACTIVE');
+//     });
+// });
