0e3fb463f6a1ab4b0db22050e2f6781077335ac3
JackHao0921 <jack02131166@gmail.com>
2023-05-21 00:16:46 +0800

    RWD的導覽列新增白色底色，並且只有滾動時會出現，停三秒時＆在最上面時消失

--- nav.js
+++ nav.js
@@ -2,8 +2,12 @@ $(document).ready(function () {
     $(window).scroll(function () {
         if ($(window).scrollTop() === 0) {
             // 導覽列陰影
-            $('.HEADER_NAV')
+            $('.HEADER_NAV, .M_HEADER_NAV')
                 .removeClass('HEADER_NAV_SHADOW ');
+
+            // RWD導覽列底色
+            $('.M_HEADER_NAV')
+                .removeClass('M_HEADER_BG_ACTIVE ');
 
             // LOGO形變
             $('.HEADERLOGO_BG_BOX')
@@ -19,8 +23,13 @@ $(document).ready(function () {
 
         } else {
             // 導覽列陰影
-            $('.HEADER_NAV')
+            $('.HEADER_NAV, .M_HEADER_NAV')
                 .addClass('HEADER_NAV_SHADOW ');
+
+            // RWD導覽列底色
+            $('.M_HEADER_NAV')
+                .addClass('M_HEADER_BG_ACTIVE ');
+
             // LOGO形變
             $('.HEADERLOGO_BG_BOX')
                 .addClass('HEADERLOGE_BG_SCROLL ')
@@ -35,35 +44,53 @@ $(document).ready(function () {
     });
 });
 
+// 畫面停止3秒自動移除RWD導覽列的底色
+// 計時器
+var timer;
 
+// 监听滚动事件
+$(window).on('scroll', resetTimer);
 
+// 开始计时器
+startTimer();
 
+// 重置计时器
+function resetTimer() {
+  clearTimeout(timer);
+  startTimer();
+}
 
-
-
-// css("display", "block") 
+// 开始计时器函数
+function startTimer() {
+  // 设置5秒后执行移除class的函数
+  timer = setTimeout(function() {
+    // 执行需要的操作，例如移除class
+    $('.M_HEADER_NAV').removeClass('M_HEADER_BG_ACTIVE');
+    $('.M_HEADER_NAV').removeClass('HEADER_NAV_SHADOW');
+  }, 2000);
+}
 
 // ========= RWD nav ==========
 $(document).ready(function () {
-  
-        $(".TOGGLE").click(function(){
-            
-  
-           if ($("#TOGGLE")){
-                $('.M_HEADER_NAV')
+
+    $(".TOGGLE").click(function () {
+
+
+        if ($("#TOGGLE")) {
+            $('.M_HEADER_NAV')
                 .toggleClass('M_HEADER_NAV_SHADOW ');
 
-                $('.M_HEADERLOGO_BG_BOX')
+            $('.M_HEADERLOGO_BG_BOX')
                 .toggleClass('M_HEADERLOGE_BG_SCROLL ')
                 .toggleClass('M_HEADERLOGE_BG_TOP ');
 
-                $('.M_HEADERLOGO_PAW')
-                    .toggleClass('M_HEADERLOGO_S ');
+            $('.M_HEADERLOGO_PAW')
+                .toggleClass('M_HEADERLOGO_S ');
 
-                $('.M_HEADER_LOGO_NAME')
-                    .toggleClass('M_HEADERNAME_S')
-           }
+            $('.M_HEADER_LOGO_NAME')
+                .toggleClass('M_HEADERNAME_S')
+        }
 
-        });
+    });
 
 });
