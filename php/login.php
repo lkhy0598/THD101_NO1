f89ad03a91dc353fd9fa93ef1fe630256562d408
JackHao0921 <jack02131166@gmail.com>
2023-06-07 15:51:51 +0800

    登入OK 可建立session

--- php/login.php
+++ php/login.php
@@ -2,34 +2,45 @@
 include('conn.php');
 
 //---------------------------------------------------
-// print_r($PDO);
+// $USERNAME = htmlspecialchars($_POST['USERNAME']);
+$EMAIL = htmlspecialchars($_POST['EMAIL']);
+$PASSWORD = htmlentities($_POST['PASSWORD']);
+
 //建立SQL語法
-$sql = "SELECT * FROM member where PHONENO = '" . $_POST["accountPhone"] . "' and PASSWORD = '" . $_POST["password"] . "'";
+$sql = "SELECT * FROM member WHERE EMAIL = ? and PASSWORD = ? ";
 
-
-
-//執行並查詢，會回傳查詢結果的物件，必須使用fetch、fetchAll...等方式取得資料
-$statement = $pdo->query($sql);
+//會自動檢查數據格式，並轉換特殊字元，再將 User Input 填入 SQL 語法中 
+$statement = $pdo->prepare($sql);
+$statement->bindValue(1, $EMAIL); //數字代表的是上面SQL語法中的第幾個問號
+$statement->bindValue(2, $PASSWORD);
+$statement->execute();
 
 //抓出全部且依照順序封裝成一個二維陣列
 $data = $statement->fetchAll();
-
-//將二維陣列取出顯示其值
-// echo $sql;
-if (count($data)>0) {
-       foreach ($data as $index => $row) {
-              echo "登入成功";
-             
+session_start();
+// 執行
+if (count($data) > 0) {
+       // 成功獲取email和password
+       foreach ($data as $row) {
+              // 抓取其使用者名稱
+              $sql = "SELECT NAME FROM member WHERE EMAIL = ?";
+              $statement = $pdo->prepare($sql);
+              $statement->bindValue(1, $EMAIL);
+              $statement->execute();
+              $nameData  = $statement->fetch();
+              // 套上名稱
+              if ($row) {
+                     // 將會員名稱儲存在 $_SESSION["memberID"] 
+                     $_SESSION["memberID"] = $nameData ['NAME']; 
+                     echo "登入成功！\n";
+                     echo "三秒後將跳轉到會員中心\n";
+                     break;
+              }else{
+                     $_SESSION["memberID"] = '使用者';
+                     break;
+              }
        }
 } else {
-       echo "登入失敗";
-       print_r ($data);
-       print_r (count($data));
-
-
-       // sleep (5);
-       // header ("location:./login.html");
-};
-
-
-?>
+       echo "登入失敗！\n";
+       echo "請檢查帳號密碼是否輸入正確！\n";
+}
